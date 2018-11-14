46fe2d5433f3ee2879c0c024eb91123fac0fa33d
Tyler Pogson <tylerpogson@outlook.com>
2018-11-13 21:55:37 -0500

    Add project files.

---
+++ WhileLoops/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace WhileLoops
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.starLabel = new System.Windows.Forms.Label();
+            this.starInput = new System.Windows.Forms.TextBox();
+            this.starButton = new System.Windows.Forms.Button();
+            this.moveButton = new System.Windows.Forms.Button();
+            this.growButton = new System.Windows.Forms.Button();
+            this.fadeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // starLabel
+            // 
+            this.starLabel.AutoSize = true;
+            this.starLabel.ForeColor = System.Drawing.Color.White;
+            this.starLabel.Location = new System.Drawing.Point(24, 37);
+            this.starLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.starLabel.Name = "starLabel";
+            this.starLabel.Size = new System.Drawing.Size(134, 20);
+            this.starLabel.TabIndex = 0;
+            this.starLabel.Text = "How Many Stars?";
+            // 
+            // starInput
+            // 
+            this.starInput.Location = new System.Drawing.Point(177, 29);
+            this.starInput.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.starInput.Name = "starInput";
+            this.starInput.Size = new System.Drawing.Size(66, 26);
+            this.starInput.TabIndex = 1;
+            // 
+            // starButton
+            // 
+            this.starButton.Location = new System.Drawing.Point(278, 23);
+            this.starButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.starButton.Name = "starButton";
+            this.starButton.Size = new System.Drawing.Size(87, 40);
+            this.starButton.TabIndex = 2;
+            this.starButton.Text = "Enter";
+            this.starButton.UseVisualStyleBackColor = true;
+            this.starButton.Click += new System.EventHandler(this.starButton_Click);
+            // 
+            // moveButton
+            // 
+            this.moveButton.Location = new System.Drawing.Point(28, 118);
+            this.moveButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.moveButton.Name = "moveButton";
+            this.moveButton.Size = new System.Drawing.Size(106, 37);
+            this.moveButton.TabIndex = 3;
+            this.moveButton.Text = "Movement";
+            this.moveButton.UseVisualStyleBackColor = true;
+            this.moveButton.Click += new System.EventHandler(this.moveButton_Click);
+            // 
+            // growButton
+            // 
+            this.growButton.Location = new System.Drawing.Point(158, 120);
+            this.growButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.growButton.Name = "growButton";
+            this.growButton.Size = new System.Drawing.Size(112, 35);
+            this.growButton.TabIndex = 4;
+            this.growButton.Text = "Grow";
+            this.growButton.UseVisualStyleBackColor = true;
+            this.growButton.Click += new System.EventHandler(this.growButton_Click);
+            // 
+            // fadeButton
+            // 
+            this.fadeButton.Location = new System.Drawing.Point(296, 120);
+            this.fadeButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.fadeButton.Name = "fadeButton";
+            this.fadeButton.Size = new System.Drawing.Size(112, 35);
+            this.fadeButton.TabIndex = 5;
+            this.fadeButton.Text = "Fade";
+            this.fadeButton.UseVisualStyleBackColor = true;
+            this.fadeButton.Click += new System.EventHandler(this.fadeButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(447, 403);
+            this.Controls.Add(this.fadeButton);
+            this.Controls.Add(this.growButton);
+            this.Controls.Add(this.moveButton);
+            this.Controls.Add(this.starButton);
+            this.Controls.Add(this.starInput);
+            this.Controls.Add(this.starLabel);
+            this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "while Loops";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label starLabel;
+        private System.Windows.Forms.TextBox starInput;
+        private System.Windows.Forms.Button starButton;
+        private System.Windows.Forms.Button moveButton;
+        private System.Windows.Forms.Button growButton;
+        private System.Windows.Forms.Button fadeButton;
+    }
+}
+
---
+++ WhileLoops/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace WhileLoops
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void starButton_Click(object sender, EventArgs e)
+        {
+            Graphics formGraphics = this.CreateGraphics();
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+            
+            //initialize
+            int counter = 1;
+            int starSize = 20;
+            int x = 50;
+            int y = 20;
+
+            //get input
+            int numStars = Convert.ToInt16(starInput.Text);
+
+            formGraphics.Clear(Color.Black);
+
+            //test
+            while (counter <= numStars)
+            {
+                //act
+                formGraphics.FillEllipse(drawBrush, starSize * counter, x, y, starSize);
+
+                //change
+                counter++; // counter = counter + 1;
+            }
+        }
+
+        private void moveButton_Click(object sender, EventArgs e)
+        {          
+            Graphics formGraphics = this.CreateGraphics();
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+
+            //initialize
+            int starSize = 10;
+            int x = 0;
+            int y = 0;
+            
+            //test
+            while (y < this.Height - starSize && x < this.Width - starSize)	
+            {
+                //act
+                formGraphics.Clear(Color.Black);
+                formGraphics.FillRectangle(drawBrush, x, y, starSize, starSize);
+                Thread.Sleep(5);
+                
+                //change
+                y = y+2;
+                x = x+2;
+            }
+        }
+
+        private void growButton_Click(object sender, EventArgs e)
+        {
+            Graphics formGraphics = this.CreateGraphics();
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+
+            //initialize
+            int pixelGrow = 1;
+            int x = 50;
+            int y = 150;
+            int starSize = 20;
+                	    
+            //test
+            while (pixelGrow < 50)	
+            {
+                //act
+                formGraphics.Clear(Color.Black);
+                formGraphics.FillRectangle(drawBrush, x - pixelGrow/2, y - pixelGrow/2, starSize + pixelGrow, starSize + pixelGrow);
+            
+                Thread.Sleep(5);
+
+                //change
+                pixelGrow++;	
+            }
+        }
+
+        private void fadeButton_Click(object sender, EventArgs e)
+        {
+            Graphics formGraphics = this.CreateGraphics();
+            SolidBrush drawBrush = new SolidBrush(Color.White);
+
+            //initialize
+            int r = 255;
+            int g = 0;
+            int b = 0;
+            int x = 50;
+            int y = 150;
+            int starSize = 50;
+
+            //test
+            while (b < 256)
+            {
+                //act
+                drawBrush.Color = Color.FromArgb(r , g , b);
+
+                formGraphics.Clear(Color.Black);
+                formGraphics.FillRectangle(drawBrush, x, y, starSize, starSize);
+
+                Thread.Sleep(10);
+
+                //change
+                b = b+5;
+                r = r-5;
+            }
+        }
+    }
+}
